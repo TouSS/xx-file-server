7e71660881b8036d9dd0488693aa930cf68034e7
TouSS <dxiaoxiang@gmail.com>
2018-12-11 17:10:44 +0800

    a miss

--- demo/pdf2pngDemo.js
+++ demo/pdf2pngDemo.js
@@ -1,6 +1,6 @@
 var im = require('imagemagick')
 
-im.convert(['-density','150','-antialias', '/home/touss/Documents/demo.ps', '-quality', '100', '/home/touss/Documents/imgs/%03d.png'], 
+im.convert(['-density','150','-antialias', '/home/touss/Documents/demo.pdf', '-quality', '100', '/home/touss/Documents/imgs/%03d.png'], 
 function(err, stdout){
   if (err) throw err;
   console.log('stdout:', stdout);
